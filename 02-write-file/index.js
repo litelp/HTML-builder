12790da8894cf3f55d30017786d5790b7f11585d
litelp <lponomareva97@yandex.ru>
2025-01-16 19:56:14 +0400

    feat: add solution for task-2

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const { stdin, stdout } = process;
+
+const pathFile = path.join(__dirname, 'text.txt');
+
+const newFile = fs.createWriteStream(pathFile);
+
+stdout.write('Hello! Enter the password: ');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() !== 'exit') {
+    newFile.write(data);
+  } else {
+    process.exit();
+  }
+});
+
+process.on('exit', () => stdout.write('\nGoodbye!'));
+process.on('SIGINT', () => process.exit());
