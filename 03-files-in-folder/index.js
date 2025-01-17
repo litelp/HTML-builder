67669e83f1e6ae07e7db21e9856bf6bd35e3b70d
litelp <lponomareva97@yandex.ru>
2025-01-17 17:11:04 +0400

    feat: add solution for task-3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+  console.log('Files in Secret-folder: ');
+  for (const file of files) {
+    if (file.isFile()) {
+      const name = file.name.split('.')[0];
+      const extension = path.extname(file.name).slice(1);
+      const pathFile = path.join(pathFolder, file.name);
+      fs.stat(pathFile, (err, stats) => {
+        if (err) console.log(err);
+        const size = stats.size / 1024;
+        console.log(`${name} - ${extension} - ${size.toFixed(3)}kb`)
+      });
+    }
+  }
+});
